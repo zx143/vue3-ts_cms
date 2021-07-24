a2614aa21a63a51f84d3be7ded6104e27203a0c4
zgq2021 <zgq15173465209@163.com>
2021-07-24 16:00:34 +0800

    style(utils): commit 提交拦截

---
+++ commitlint.config.js
@@ -0,0 +1,10 @@
+/*
+ * @Description:
+ * @Author: zgq
+ * @Date: 2021-07-24 15:47:11
+ * @LastEditors: zgq
+ * @LastEditTime: 2021-07-24 15:48:33
+ */
+module.exports = {
+  extends: ['@commitlint/config-conventional']
+}
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,13 @@
+/*
+ * @Description:
+ * @Author: zgq
+ * @Date: 2021-07-24 12:16:14
+ * @LastEditors: zgq
+ * @LastEditTime: 2021-07-24 15:56:00
+ */
 import { createApp } from 'vue'
 import App from './App.vue'
 
 createApp(App).mount('#app')
+
+console.log(13)
