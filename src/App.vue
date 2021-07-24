1ad40a4bd9ba5b8c0b0b415e478dff3f3ec47b8c
zgq2021 <zgq15173465209@163.com>
2021-07-24 15:21:09 +0800

    test code format

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,25 @@
+<!--
+ * @Description:
+ * @Author: zgq
+ * @Date: 2021-07-24 12:16:14
+ * @LastEditors: zgq
+ * @LastEditTime: 2021-07-24 15:18:45
+-->
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import { defineComponent } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
 
 export default defineComponent({
-  name: "App",
+  name: 'App',
   components: {
-    HelloWorld,
-  },
-});
+    HelloWorld
+  }
+})
 </script>
 
 <style lang="less">
@@ -20,6 +27,7 @@ export default defineComponent({
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
